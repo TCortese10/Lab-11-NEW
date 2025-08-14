5dd2bc55319f818beeb041df9c587ec71817921b
Turner Cortese <turner.cortese@yahoo.com>
2025-08-14 16:22:34 -0400

    created class store

---
+++ script.js
@@ -0,0 +1,68 @@
+class ProductProperties{
+    // constructor method to set properties
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    // method to return the product in stock
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+    // displays product information
+    toString () {
+        console.log(`Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`);
+    }
+        // static method created
+    static applyDiscount(products, discount) {
+        for (let product of products) { // goes through each item and then applies the discount
+            product.price = product.price - (product.price * discount)
+        }
+        
+    }
+
+}
+
+// subclass of productProperties
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity); // calls the parent constructor
+        this.expirationDate = expirationDate; // add expiration date
+    }
+
+    toString () { //overwrites and adds expiration date
+        console.log(`Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`);
+    }
+
+}
+
+// two instances of the PerishableProcuts class
+let item1 = new PerishableProductProperties("Banana", 25, 5, "2025-05-10");
+item1.toString();
+
+let item2 = new PerishableProductProperties("Canned Soup", 5, 108, "2032-03-21");
+item2.toString();
+
+//created new items and array that applies a discount
+
+let item3 = new ProductProperties("Beans", 15, 12);
+let item4 = new ProductProperties("Peas", 22.50, 23);
+let item5 = new ProductProperties("Carrots", 8.50, 455);
+let item6 = new ProductProperties("Beans", 15.25, 233);
+
+ProductProperties.applyDiscount([item3, item4, item5, item6], 0.2);
+//outputs discounted items
+console.log("");
+console.log("Discounted Items");
+console.log("----------------");
+item3.toString();
+item4.toString();
+item5.toString();
+item6.toString();
+
+
+// create a store inventory
+
+class store {
+    
+}
